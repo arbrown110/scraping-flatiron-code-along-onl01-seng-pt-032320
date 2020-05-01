d90d13a4464a19f004d6c237237ff7392f76976d
chic-caller-7520 <arbrown110@gmail.com>
2020-05-01 21:09:32 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,17 +2,16 @@ class Course
   attr_accessor :title, :schedule,:description
   
   @@all= []
-  
-  def intialize
+def initialize
     @@all << self
   end
-  
+ 
   def self.all
     @@all
   end
-  
+ 
   def self.reset_all
     @@all.clear
-  end  
-    
+  end
+ 
 end
